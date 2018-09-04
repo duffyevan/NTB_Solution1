b2f4ed111285a1b33128d621a02c53b61e464fd0
c-patel <c-patel@ntb.ch>
2018-09-04 13:55:24 +0200

    Updated test cases to test avarage file size for SPS and find days left before full

--- test.py
+++ test.py
@@ -10,9 +10,6 @@ from email.mime.multipart import MIMEMultipart
 from EMailLib import *
 
 
-
-
-
 config = configparser.ConfigParser()
 config.read('collection.conf')
 
@@ -24,19 +21,34 @@ destination = config['DEFAULT']['default_destination']
 ftp = FTP('10.0.0.99')
 ftp.login(user=user, passwd=password)
 
+#file Size and days left calcualtor
+SD_size = int(config['EMAIL']['SD_size'])
 sps = SPSLib(ftp)
-totalusage = sps.get_total_size('/')
+tupleresult = sps.get_total_size('/')
+totalusage = tupleresult[0]
 totalusageMB = round(totalusage/(1024*1024), 2) 
+avg_size = round(totalusageMB/tupleresult[1], 2)
+size_left = round((SD_size - totalusageMB), 2)
+days_left = int(size_left/avg_size)
+
+print ('print tuple: ' + str(tupleresult))
 print ('Total Usage In MB: ' + str(totalusageMB) + 'MB')
+print ('Avarage file size per day in MB: ' + str(avg_size) + 'MB')
+print ('Size left before full: ' + str(size_left) + 'MB')
+print ('Days left before storage is full: ' + str(days_left) + ' days')
+
 
 sps.close_connection()
 
-message = EMail(config['EMAIL']['username'], config['EMAIL']['recipient'])
-message.set_subject("Test Email Send From Library")
-message.set_body("The current SPS with the address of: 111111 has used up " + str(totalusageMB) + " MB.")
 
-sender = EMailSender(config['EMAIL']['smtp_server'], config['EMAIL']['smtp_port'])
-sender.login(config['EMAIL']['username'],config['EMAIL']['password'])
-sender.send_message(message)
-sender.quit()
+#Emial Section:
+
+# message = EMail(config['EMAIL']['username'], config['EMAIL']['recipient'])
+# message.set_subject("Test Email Send From Library")
+# message.set_body("The current SPS with the address of: 111111 has used up " + str(totalusageMB) + " MB.")
+
+# sender = EMailSender(config['EMAIL']['smtp_server'], config['EMAIL']['smtp_port'])
+# sender.login(config['EMAIL']['username'],config['EMAIL']['password'])
+# sender.send_message(message)
+# sender.quit()
 

22b4a4ff22d6f6eae13b9c71d511be91634d6434
c-duffy <c-duffy@ntb.ch>
2018-09-05 15:58:46 +0200

    Iterates through addresses and retrys based on configuration values

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ from SPSLib import SPSLib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from EMailLib import *
+import time
 
 
 config = configparser.ConfigParser()
@@ -17,55 +18,73 @@ addresses = (config['DEFAULT']['addresses']).split(',')
 user = config['DEFAULT']['username']
 password = config['DEFAULT']['password']
 destination = config['DEFAULT']['default_destination']
+numretries = int(config['DEFAULT']['number_connection_retries'])
+retrydelay = int(config['DEFAULT']['connection_retry_delay'])
 
-ftp = FTP('10.0.0.99')
-ftp.login(user=user, passwd=password)
+ftp = 0
+for address in addresses:
+    success = False
+    for n in range(0, numretries):
+        try:
+            ftp = FTP(address)
+            ftp.login(user=user, passwd=password)
+            success = True
+        except:
+            print('Connection Attempt ' + str(n+1) + ' to ' + address + ' Failed')
+            time.sleep(retrydelay)
 
-#file Size and days left calcualtor
-SD_size = int(config['EMAIL']['SD_size'])
-sps = SPSLib(ftp)
-tupleresult = sps.get_total_size('/')
-totalusage = tupleresult[0]
-totalusageMB = round(totalusage/(1024*1024), 2) 
-avg_size = round(totalusageMB/tupleresult[1], 2)
-size_left = round((SD_size - totalusageMB), 2)
-days_left = int(size_left/avg_size)
-
-print ('print tuple: ' + str(tupleresult))
-print ('Total Usage In MB: ' + str(totalusageMB) + 'MB')
-print ('Avarage file size per day in MB: ' + str(avg_size) + 'MB')
-print ('Size left before full: ' + str(size_left) + 'MB')
-print ('Days left before storage is full: ' + str(days_left) + ' days')
-
-
-sps.close_connection()
+    if not success:
+        print('Error Connecting To PLC at ' + address)
+        continue
+        # sys.exit(1)
 
 
-# #Emial Section:
 
-names, emails = EMail.get_contacts('email_templates/email_addresses.txt') # read contacts
-message_template = EMail.read_template('email_templates/OneDayWarning.temp')
+    #file Size and days left calcualtor
+    SD_size = int(config['EMAIL']['SD_size'])
+    sps = SPSLib(ftp)
+    tupleresult = sps.get_total_size('/')
+    totalusage = tupleresult[0]
+    totalusageMB = round(totalusage/(1024*1024), 2) 
+    avg_size = round(totalusageMB/tupleresult[1], 2)
+    size_left = round((SD_size - totalusageMB), 2)
+    days_left = int(size_left/avg_size)
+
+    print ('print tuple: ' + str(tupleresult))
+    print ('Total Usage In MB: ' + str(totalusageMB) + 'MB')
+    print ('Avarage file size per day in MB: ' + str(avg_size) + 'MB')
+    print ('Size left before full: ' + str(size_left) + 'MB')
+    print ('Days left before storage is full: ' + str(days_left) + ' days')
 
 
-sender = EMailSender(config['EMAIL']['smtp_server'], config['EMAIL']['smtp_port'])
-sender.login(config['EMAIL']['username'],config['EMAIL']['password'])
+    sps.close_connection()
 
-# For each contact, send the email:
-for name, email in zip(names, emails):
-    msg = MIMEMultipart()       # create a message
 
-    # add in the actual person name to the message template
-    body = message_template.substitute(user=name.title(), address=addresses, days=days_left)
-    
-    message = EMail(config['EMAIL']['username'], email)
-    message.set_subject("Test Email Send From Library")
-    message.set_body(body)
+    #Emial Section:
 
-    # send the message via the server set up earlier.
-    # sender.send_message(message)
+    names, emails = EMail.get_contacts('email_templates/email_addresses.txt') # read contacts
+    message_template = EMail.read_template('email_templates/OneDayWarning.temp')
 
-    # Prints out the message body for our sake
-    print(body)
-    
-# Terminate the SMTP session and close the connection
-sender.quit()
+
+    sender = EMailSender(config['EMAIL']['smtp_server'], config['EMAIL']['smtp_port'])
+    sender.login(config['EMAIL']['username'],config['EMAIL']['password'])
+
+    # For each contact, send the email:
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()       # create a message
+
+        # add in the actual person name to the message template
+        body = message_template.substitute(user=name.title(), address=address, days=days_left)
+        
+        message = EMail(config['EMAIL']['username'], email)
+        message.set_subject("Test Email Send From Library")
+        message.set_body(body)
+
+        # send the message via the server set up earlier.
+        # sender.send_message(message)
+
+        # Prints out the message body for our sake
+        print(body)
+        
+    # Terminate the SMTP session and close the connection
+    sender.quit()

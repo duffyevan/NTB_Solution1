3f496d8a139c5db004777c25f544b5d6f4b646d5
c-duffy <c-duffy@ntb.ch>
2018-09-11 16:39:11 +0200

    Hella documentation

--- EMailLib.py
+++ EMailLib.py
@@ -6,24 +6,62 @@ from string import Template
 
 
 class EMail:
+    """An Email Message To Be Sent Via an Email Sender
+    """
+
     def __init__(self, from_address, to_address):
+        """The Constructor
+        
+        Arguments:
+            from_address {string} -- The email address to send the email from 
+            to_address {string} -- The email address to send the email to
+        """
         self.message = MIMEMultipart()
         self.message["To"] = to_address
         self.message["From"] = from_address
 
     def set_from(self, address):
+        """Set the from address
+        
+        Arguments:
+            address {string} -- From address
+        """
         self.message["From"] = address
 
     def set_to(self, address):
+        """Set the to address
+        
+        Arguments:
+            address {string} -- To Address
+        """
+
         self.message["To"] = address
 
     def set_subject(self, subject):
+        """Set the subject of the email
+        
+        Arguments:
+            subject {string} -- The Subject
+        """
+
         self.message["Subject"] = subject
 
     def set_body(self, text):
+        """Set the body of the email to some text
+        
+        Arguments:
+            text {string} -- Text to set the email body to
+        """
+
         self.message.attach(MIMEText(text))
 
     def set_body_html(self, html):
+        """Set the body of the emial to some html and package it up correctly
+        
+        Arguments:
+            html {string} -- String of html code to be packaged and sent as the body
+        """
+
         self.message.attach(MIMEText(html, 'html'))
 
     def get_from(self):
@@ -39,9 +77,14 @@ class EMail:
 
     @staticmethod
     def read_template(filename):
-        """
-        Returns a Template object comprising the contents of the 
+        """Returns a Template object comprising the contents of the 
         file specified by filename.
+
+        Arguments:
+            filename {string} -- Path to the tempalte file to load
+
+        Returns:
+            Template -- The template object to be used to construct emails
         """
         with open(filename, 'r', encoding='utf-8') as template_file:
             template_file_content = template_file.read()
@@ -49,9 +92,14 @@ class EMail:
 
     @staticmethod
     def get_contacts(filename):
-        """
-        Return two lists names, emails containing names and email addresses
+        """Return two lists names, emails containing names and email addresses
         read from a file specified by filename.
+        
+        Arguments:
+            filename {string} -- Path to the tempalte file to load
+
+        Returns:
+            (names, emails) -- Tuple of contact names and corresponding contact emails
         """
         
         names = []
@@ -64,15 +112,41 @@ class EMail:
 
 
 class EMailSender:
+    """Wrapper Over an SMTP Server to Send EMail Objects
+    """
+
     def __init__(self, server, port):
+        """The Constructor
+        
+        Arguments:
+            server {string} -- FQDN of the SMTP server
+            port {int} -- Port number for the TLS version of SMTP
+        """
+
         self.smtp = smtplib.SMTP(server, port=port)
         self.smtp.starttls()
     
     def login(self, username, password):
+        """Call the login function on the SMTP server
+        
+        Arguments:
+            username {string} -- Username to use to log into the SMTP server
+            password {string} -- Password for given username
+        """
+
         self.smtp.login(username, password)
 
     def send_message(self, message):
+        """Send a given message via the SMTP server
+        
+        Arguments:
+            message {EMailLib.Email} -- An email object representing the message to be sent
+        """
+
         self.smtp.sendmail(message.get_from(), message.get_to(), message.message.as_string())
 
     def quit(self):
+        """Quit the SMTP connection
+        """
+
         self.smtp.quit()
--- HostpointLib.py
+++ HostpointLib.py
@@ -1,23 +1,53 @@
 from ftplib import FTP
 import os
 
+
 class HostpointClient:
     def __init__(self, hostname, username, password):
+        """The Constructor
+        
+        Arguments:
+            hostname {string} -- hostname of the HostPoint ftp server
+            username {string} -- username for the HostPoint ftp server
+            password {string} -- password for said user
+        """
+
         self.client = FTP(hostname)
         self.client.login(username, password)
 
     def upload_file(self, filename):
+        """Upload a single file from the local storage to the FTP server
+        
+        Arguments:
+            filename {string} -- Path to the file on the local storage to upload
+        """
+
         file = open(filename, 'rb')
         self.client.storbinary('STOR %s' % os.path.basename(filename), file)
         file.close()
 
     def upload_files(self, filelist):
+        """Upload a list of files from the local storage to the FTP server
+        
+        Arguments:
+            filelist {list[string]} -- A list of paths to files on the local storage
+        """
+
         for file in filelist:
             self.upload_file(file)
 
     def upload_all_files_in_directory(self, directory):
+        """Upload all the files in a given directory on the local storage to the FTP server
+        
+        Arguments:
+            directory {string} -- Path to the directory on the local storage
+        """
+
         self.upload_files([os.path.join(directory, file) for file in os.listdir(directory)])
 
 
     def close(self):
+        """Close the connection with HostPoint server
+        """
+
         self.client.close()

b8d79afe4587a1232495adc7ea1c68390d4f20ba
c-duffy <c-duffy@ntb.ch>
2018-09-13 17:02:37 +0200

    Working On UI, Not Fully Functional

---
+++ frontend/ConfigManager.py
@@ -0,0 +1,16 @@
+import configparser
+import os
+
+
+class ConfigManager:
+    def __init__(self, filename):
+        self.__config = configparser.ConfigParser()
+        self.__config.read(filename)
+
+        self.SPSaddresses = (self.__config['DEFAULT']['addresses']).split(',')
+        self.SPSuser = self.__config['DEFAULT']['username']
+        self.SPSpassword = self.__config['DEFAULT']['password']
+        self.downloaddestination = self.__config['DEFAULT']['default_destination'].replace("~", "c:\\Users\\" + os.getlogin())
+        self.connectionnumretries = int(self.__config['DEFAULT']['number_connection_retries'])
+        self.connectionretrydelay = int(self.__config['DEFAULT']['connection_retry_delay'])
+        self.SPSfullwarningdays = int(self.__config['EMAIL']['warning_days'])

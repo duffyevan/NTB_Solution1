04cf154cc980279b5362a0c2509a4651e7cb2910
c-duffy <c-duffy@ntb.ch>
2018-09-17 18:41:56 +0200

    Completed function to read and parse a TSV file into a dictionary of values

--- Analysis.py
+++ Analysis.py
@@ -4,7 +4,8 @@ def read_tsv(filename):
     file = open(filename, 'r')
     lines = file.readlines()
     title_line_number = find_first_non_empy_line(lines)
-    titles = lines[title_line_number].replace('\n','').split()
+    # titles = lines[title_line_number].replace('\n','').split()
+    titles = parse_line(lines[title_line_number].replace('\n', ''))
 
     dictionary = dict()
 
@@ -12,17 +13,27 @@ def read_tsv(filename):
         dictionary[title] = []
 
     for index in range(title_line_number+1, len(lines)):
-        # data = lines[index].replace('\n','').split()
-        data = re.compile("\t").split(lines[index].replace('\n',''))
+        data = parse_line(lines[index].replace('\n', ''))
         for d_index in range(0,len(data)):
             dictionary[titles[d_index]].append(data[d_index])
     
     return dictionary
     
+def parse_line(line):
+    prev_i = 0
+    i = 0
+    entries = []
+    while i <= len(line):
+        if i >= len(line) or line[i] in "\t\n":
+            entries.append(line[prev_i:i])
+            prev_i = i + 1
+        i += 1
+    return entries
+
 def find_first_non_empy_line(lines):
     ret = 0
     while lines[ret].replace('\n','') is "":
         ret += 1
     return ret
 
-print(read_tsv("test.txt"))
+# print(read_tsv('C:\\Users\\c-duffy\\Documents\\F016_20171203_000002.xls')['01_Saus1'][3])
